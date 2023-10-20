69b38596f1fc51db4ef0b30f70a6f3e1cb03e5c2
Naymul-NN <naymulislam108@gmail.com>
2023-10-20 11:18:07 +0600

    post more data for brand

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const client = new MongoClient(uri, {
   }
 });
 
-
 async function run() {
   try {
     await client.connect();
@@ -117,15 +116,7 @@ async function run() {
       res.send(result);
   })
    
-    // delete
-    // app.delete("/users/:id", async(req,res)=>{
-    //     const id = req.params.id;
-    //     const query = {
-    //         _id: new ObjectId(id),
-    //     };
-    //     const result = await userCollection.deleteOne(query);
-    //     res.send(result);
-    // })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
